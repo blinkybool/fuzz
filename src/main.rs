91e5bd65cb1289b67eda13d95de6ba5a5cab3556
Billy Snikkers <billyprice98is@gmail.com>
2023-05-30 00:13:03 +0200

    basic static site generator

---
+++ src/main.rs
@@ -0,0 +1,130 @@
+use axum::Router;
+use std::{fs, io::{self, Write}, net::SocketAddr, path::Path};
+use tower_http::services::ServeDir;
+use tower_livereload::LiveReloadLayer;
+use notify::{Watcher, RecommendedWatcher};
+
+mod templates;
+const CONTENT_DIR: &str = "content";
+const PUBLIC_DIR: &str = "public";
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+		rebuild_site().expect("Rebuilding site");
+
+		let livereload = LiveReloadLayer::new();
+		let reloader = livereload.reloader();
+		let app = Router::new()
+			.nest_service("/", ServeDir::new(PUBLIC_DIR))
+			.layer(livereload);
+
+
+		// let mut content_watcher = RecommendedWatcher::new(move |_| rebuild_site().expect("Rebuilding site"), notify::Config::default())?;
+		let mut content_watcher = RecommendedWatcher::new(move |result: notify::Result<notify::Event>| {
+
+			match result {
+				Err(e) => println!("error: {}", e),
+				Ok(event) => {
+					let event_kind = match event.kind {
+						notify::EventKind::Create(_) => "Create",
+						notify::EventKind::Modify(_) => "Modify",
+						notify::EventKind::Remove(_) => "Remove",
+						_ => "Other",
+				};
+					println!("kind: {}", event_kind);
+					for path in event.paths {
+						println!("path changed: {}", path.display());
+					};
+				}
+			}
+			rebuild_site().expect("Rebuilding site");
+	}, notify::Config::default().with_compare_contents(true))?;
+		content_watcher.watch(Path::new(CONTENT_DIR), notify::RecursiveMode::Recursive)?;
+		let mut public_watcher = RecommendedWatcher::new(move |_| reloader.reload(), notify::Config::default())?;
+		public_watcher.watch(Path::new(PUBLIC_DIR), notify::RecursiveMode::Recursive)?;
+
+		let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+		println!("serving site on http://{}", addr);
+		axum::Server::bind(&addr)
+			.serve(app.into_make_service())
+			.await?;
+
+		Ok(())
+}
+
+fn rebuild_site() -> Result<(), anyhow::Error> {
+	print!("rebuilding...");
+	io::stdout().lock().flush().unwrap();
+
+	let _ = fs::remove_dir_all(PUBLIC_DIR);
+	let _ = fs::create_dir_all(Path::new(PUBLIC_DIR));
+
+	let markdown_files: Vec<String> = walkdir::WalkDir::new(CONTENT_DIR)
+		.into_iter()
+		.filter_map(|e| e.ok())
+		.filter(|e| e.path().display().to_string().ends_with(".md"))
+		.map(|e| e.path().display().to_string())
+		.collect();
+	let mut html_files = Vec::with_capacity(markdown_files.len());
+
+	for file in &markdown_files {
+		let mut html = templates::HEADER.to_owned();
+		let markdown = fs::read_to_string(&file)?;
+		let parser = pulldown_cmark::Parser::new_ext(&markdown, pulldown_cmark::Options::all());
+
+		let mut body = String::new();
+		pulldown_cmark::html::push_html(&mut body, parser);
+
+		html.push_str(templates::render_body(&body).as_str());
+		html.push_str(templates::FOOTER);
+
+		let html_file = file
+			.replace(CONTENT_DIR, PUBLIC_DIR)
+			.replace(".md", ".html");
+		fs::write(&html_file, html)?;
+
+		html_files.push(html_file);
+	}
+
+	write_index(html_files, PUBLIC_DIR)?;
+
+	// Just copy over non-markdown stuff
+	for entry in walkdir::WalkDir::new(CONTENT_DIR) {
+		let entry = entry?;
+		let path = entry.path();
+		let path_str = entry.path().display().to_string();
+		if entry.path().is_file() && !path_str.ends_with(".md") {
+			
+			let dest_path = path_str
+				.replace(CONTENT_DIR, PUBLIC_DIR);
+
+			// Using copy modifies the file, triggering another build
+			// So we read, then write, instead
+			let file_content = fs::read_to_string(path)?;
+			fs::write(dest_path, file_content)?;
+		}
+	}
+	
+	print!("done\n");
+	Ok(())
+}
+
+fn write_index(files: Vec<String>, output_dir: &str) -> Result<(), anyhow::Error> {
+	let mut html = templates::HEADER.to_owned();
+	let body = files
+		.into_iter()
+		.map(|file| {
+			let file = file.trim_start_matches(output_dir);
+			let title = file.trim_start_matches("/").trim_end_matches(".html");
+			format!(r#"<a href="{}">{}</a>"#, file, title)
+		})
+		.collect::<Vec<String>>()
+		.join("<br />\n");
+
+	html.push_str(templates::render_body(&body).as_str());
+	html.push_str(templates::FOOTER);
+
+	let index_path = Path::new(&output_dir).join("index.html");
+	fs::write(index_path, html)?;
+	Ok(())
+}
---
+++ src/templates.rs
@@ -0,0 +1,31 @@
+pub const HEADER: &str = r#"<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+
+"#;
+
+// <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
+
+pub fn render_body(body: &str) -> String {
+    format!(
+        r#"  <body>
+    <nav>
+        <a href="/">Home</a>
+    </nav>
+    <br />
+    <article class="post">
+    {}
+    </article>
+  </body>"#,
+        body
+    )
+}
+
+pub const FOOTER: &str = r#"
+</html>
+"#;
