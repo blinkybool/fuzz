a51646d34e1f10b1d0668df652a9261f226ec8d1
Billy Snikkers <billyprice98is@gmail.com>
2023-05-31 10:41:08 +0200

    indentation

--- src/main.rs
+++ src/main.rs
@@ -10,29 +10,29 @@ const PUBLIC_DIR: &str = "public";
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-		rebuild_site().expect("Rebuilding site");
+	rebuild_site().expect("Rebuilding site");
 
-		let livereload = LiveReloadLayer::new();
-		let reloader = livereload.reloader();
-		let app = Router::new()
-			.nest_service("/", ServeDir::new(PUBLIC_DIR))
-			.layer(livereload);
+	let livereload = LiveReloadLayer::new();
+	let reloader = livereload.reloader();
+	let app = Router::new()
+		.nest_service("/", ServeDir::new(PUBLIC_DIR))
+		.layer(livereload);
 
-		let mut content_watcher = RecommendedWatcher::new(move |_| {
-			rebuild_site().expect("Rebuilding site")
-		}, notify::Config::default())?;
-		content_watcher.watch(Path::new(CONTENT_DIR), notify::RecursiveMode::Recursive)?;
+	let mut content_watcher = RecommendedWatcher::new(move |_| {
+		rebuild_site().expect("Rebuilding site")
+	}, notify::Config::default())?;
+	content_watcher.watch(Path::new(CONTENT_DIR), notify::RecursiveMode::Recursive)?;
 
-		let mut public_watcher = RecommendedWatcher::new(move |_| reloader.reload(), notify::Config::default())?;
-		public_watcher.watch(Path::new(PUBLIC_DIR), notify::RecursiveMode::Recursive)?;
+	let mut public_watcher = RecommendedWatcher::new(move |_| reloader.reload(), notify::Config::default())?;
+	public_watcher.watch(Path::new(PUBLIC_DIR), notify::RecursiveMode::Recursive)?;
 
-		let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-		println!("Serving site at http://{}", addr);
-		axum::Server::bind(&addr)
-			.serve(app.into_make_service())
-			.await?;
+	let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+	println!("Serving site at http://{}", addr);
+	axum::Server::bind(&addr)
+		.serve(app.into_make_service())
+		.await?;
 
-		Ok(())
+	Ok(())
 }
 
 fn rebuild_site() -> Result<(), anyhow::Error> {
--- src/templates.rs
+++ src/templates.rs
@@ -1,29 +1,26 @@
 pub const HEADER: &str = r#"<!DOCTYPE html>
 <html lang="en">
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="styles.css" />
-  </head>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="styles.css" />
+	</head>
 
 "#;
 
-// <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
-
 pub fn render_body(body: &str) -> String {
-    format!(
-        r#"  <body>
-    <nav>
-        <a href="/">Home</a>
-    </nav>
-    <br />
-    <article class="post">
-    {}
-    </article>
-  </body>"#,
-        body
-    )
+	format!(r#" 
+	<body>
+		<nav>
+			<a href="/">Home</a>
+		</nav>
+		<br />
+		<article class="post">
+		{}
+		</article>
+	</body>"#,
+	body)
 }
 
 pub const FOOTER: &str = r#"
